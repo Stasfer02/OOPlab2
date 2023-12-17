0093a4d4af6769f411fb53db629931f3644ba71b
Stasfer02 <stanferguson2002@gmail.com>
2023-12-17 22:28:01 +0100

    Finished p1

---
+++ part_1/src/mastermind.py
@@ -0,0 +1,110 @@
+"""
+the mastermind model for lab 2 of Object Oriented Programming
+Group 48: Stan Ferguson (S4674367), Jelle Molenaar (S4807243)
+"""
+
+import numpy as np
+
+import sys
+sys.path.append ("tests")
+from tests import testFile
+from typing import List
+
+tests = testFile.Tests()
+
+#starting the program, we provide the user with the rules and let them input the amount of turns that is allowed.
+print("""
+Welcome to this coded version of mastermind. The rules are as follows:
+\n the computer will generate a 4-color code that you need to break. Every turn, you can propose a combination of 4 colors and the computer will respond with: 
+\n \t1. The amount of colors that are correct and in the right place (if any).
+\n \t2. The amount of colors that are correct but in the wrong place (if any).
+\n It will NOT tell you which of the colors you proposed are correct. Note that colors can be used multiple times.
+\n 4. The possible colors (with corresponding inputs) are: White 'w', Black 'k', Yellow 'y', Green 'g', Red 'r' and Blue 'b'.
+\n An example input could be: "wygb"
+\n\n Now, please enter the amount of turns that you would like as the limit (you could start with 20 if you don't know):
+""")
+
+#taking the limit and testing it's type and value
+limit = int(input())
+tests.test_limit_input_type(limit)
+tests.test_limit_input(limit)
+print("The limit has been set to: ",limit)
+
+#all valid inputs for the codes
+valid = ['w','k','y','g','r','b']
+
+def create_target(valid: List[str]):
+    target = ['','','','']
+    for i in range(len(target)):
+        target[i] = valid[np.random.randint(0,6)]
+    return target
+
+#creating our target code and testing it
+target = create_target(valid)
+tests.test_target_length(target)
+tests.test_target_inputs(target)
+
+print("The code has been generated, good luck with cracking it. Enter your first proposal:")
+
+def found_solution(proposal,target):
+    for i in range(len(proposal)):
+        if proposal[i] != target[i]:
+            return False
+    return True
+
+def evaluate_proposal(proposal,target):
+    amountCorrect = 0
+    newTarget = ['','','','']
+    newProposal = ['','','','']
+    for i in range(len(target)):
+        newTarget[i] = target[i]
+        newProposal[i] = proposal[i]
+
+    for i in range(len(target)):
+        if proposal[i] == target[i]:
+            del newTarget[i-amountCorrect]
+            del newProposal[i-amountCorrect]
+            amountCorrect+=1
+        
+    amountWrong = 0
+    idx = 0
+    while idx < len(newProposal):
+        for k in range(len(newTarget)):
+           #print(" entered loop with \n new Target: ",newTarget, "\n new Proposal: ",newProposal, "\n -- k,idx = ",k,idx,"\namount wrong: ",amountWrong,"\n------------------")
+           if newProposal[idx] == newTarget[k]:
+                del newTarget[k]
+                amountWrong+=1
+                break
+        idx+=1
+    
+    tests.test_correct_positions(amountCorrect,target,proposal)
+    tests.test_wrong_positions(amountWrong,target,proposal)
+    #printing the outcome:
+    print("The outcome of your first guess is as follows: \n Amount of colors in the right place: \t", amountCorrect, "\n Amount of colors in the wrong place: \t", amountWrong)
+
+proposal = list(input())
+tests.test_proposal_inputs(proposal)
+tests.test_proposal_length(proposal)
+
+iterations = 1
+while not found_solution(proposal,target) and iterations < limit:
+    #making sure we did not exceed the limit
+    tests.test_exceeded_limit(iterations, limit)
+
+    #doing the evaluation and returning the values for right and wrong colors
+    evaluate_proposal(proposal,target)
+
+    #taking in a new proposal
+    proposal = list(input())
+    tests.test_proposal_inputs(proposal)
+    tests.test_proposal_length(proposal)
+
+    iterations +=1
+
+if found_solution(proposal,target):
+    tests.test_found_solution(target,proposal)
+    print("----------WON----------\n Congratulations, you have found the winning solution within the turn limit!")
+    print("The final solution was indeed: ", target[0],target[1],target[2],target[3])
+else:
+    print("\n \n----------LOST---------- \n Unfortunately, the turn limit has been reached and you have not found the solution. \n Consider trying again with a higher turn limit.")
+    print("the final solution was: ", target[0],target[1],target[2],target[3])
---
+++ part_1/src/tests/testFile.py
@@ -0,0 +1,88 @@
+"""
+This is the test file for lab 2 of Object Oriented Programming. 
+Group 48: Stan Ferguson (S4674367), Jelle Molenaar (S4807243)
+
+Structure of the input:
+there are 6 possible colors: White (w), Black (k), Yellow (y), Green (g), Red (r), Blue (b)
+--- duplicates are allowed ---
+
+Codemaker (e.g. the computer), creates a code that needs to be broken. TEST
+"""
+
+import unittest
+from typing import List
+
+class Tests(unittest.TestCase):
+    #test class
+    def test_target_length(self, x: List[str]):
+        #testing if the code is 4 inputs
+        self.assertIs(len(x), 4, "The target code is not of length 4.")
+    
+    def test_target_inputs(self, x: List[str]):
+        valid = ['w','k','y','g','r','b']
+        for i in x:
+            self.assertIn(i,valid, "The target code does not contain valid inputs.")
+    
+    def test_proposal_length(self, x: List[str]):
+        self.assertIs(len(x),4, "the proposal code is not of length 4.")
+
+    def test_proposal_inputs(self, x: List[str]):
+        valid = ['w','k','y','g','r','b']
+        for i in x:
+            self.assertIn(i,valid, "the proposal code does not contain valid inputs.")
+    
+    def test_exceeded_limit(self, iterations: int, limit: int):
+        self.assertLess(iterations,limit,"The limit of iterations was exceeded." )
+    
+    def test_correct_positions(self, correctPosition: int, target: List[str], proposal: List[str]):
+        amount = 0
+
+        for i in range(len(target)):
+            if target[i] == proposal[i]:
+                amount+=1
+        self.assertEqual(amount,correctPosition, "The calculated amount of correct positions is not correct.")
+
+    def test_wrong_positions(self, wrongPosition: int, target: List[str], proposal: List[str]):
+        # we need to remove the correct positions first, otherwise we get false returns. for example:
+        # if the target is ['w','r','b','b'] and the proposal is ['r','w','r','r'] it should not return 3 wrong positions for
+        # all 'r' in the proposal, but only one. So we first remove the correct positions,
+        # then we remove target values if they have been predicted.
+        amountCorrect = 0
+        newTarget = ['','','','']
+        newProposal = ['','','','']
+        for i in range(len(target)):
+            newTarget[i] = target[i]
+            newProposal[i] = proposal[i]
+
+        for i in range(len(target)):
+            if proposal[i] == target[i]:
+                del newTarget[i-amountCorrect]
+                del newProposal[i-amountCorrect]
+                amountCorrect+=1
+        
+        amountWrong = 0
+        idx = 0
+        while idx < len(newProposal):
+            for k in range(len(newTarget)):
+                #print(" entered loop with \n new Target: ",newTarget, "\n new Proposal: ",newProposal, "\n -- k,idx = ",k,idx,"\namount wrong: ",amountWrong,"\n------------------")
+                if newProposal[idx] == newTarget[k]:
+                    del newTarget[k]
+                    amountWrong+=1
+                    break
+            idx+=1
+        #print("final evaluation: \n new target: ", newTarget, "\n new proposal: ",newProposal, "\ncorrect positions: ", amountCorrect, "\n Wrong positions: ", amountWrong)
+        self.assertEqual(amountWrong, wrongPosition, "The calculated amount of wrong positions is not correct.")
+
+    def test_found_solution(self, target: List[str], proposal: List[str]):
+        correct = 0
+        for i in range(len(target)):
+            if target[i] == proposal[i]:
+                correct+=1
+        self.assertEqual(correct,4, "the found solution is not the correct solution.")
+
+    def test_limit_input_type(self, limit):
+        self.assertIsInstance(limit, int, "input variable is not an integer.")
+
+    def test_limit_input(self,limit: int):
+        #the maximum attempts is variable, but has to stay below 100
+        self.assertLess(limit,100, "the limit boundary exceeds the maximum value of 100.")
